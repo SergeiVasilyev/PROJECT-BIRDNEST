f24350d5428d3fcb97a71ae2240717cfaa2e6096
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-12-19 15:44:24 +0200

    class xml_parsre  added

--- birdnest.py
+++ birdnest.py
@@ -1,28 +1,75 @@
+import enum
 import math
 import requests
 import xml.etree.ElementTree as ET
 
-x = requests.get('http://assignments.reaktor.com/birdnest/drones')
+# position = [250000, 250000]
+# position = [400000, 400000]
+
+# x = requests.get('http://assignments.reaktor.com/birdnest/drones')
 # print(x.content)
 
-# tree = ET.parse('test.xml') # Read from file
-tree = ET.fromstring(x.content) # Read from string
-# tree = ET.parse('test.xml')
-# tree = ET.parse('test.xml')
-for child in tree.iter('*'):
-    print(child.tag, child.attrib, child.text)
-    
+# tree = ET.fromstring(x.content) # Read from string
+# for child in tree.iter('*'):
+#     print(child.tag, child.attrib, child.text)
+
+# for drone in tree.findall('drone'):
+#     print(drone.find('serialNumber').text)
+
+# capture = tree.findall('capture')
+# for el in capture:
+#     droneEls = el.findall('drone')
+#     print(droneEls)
+#     for drn in droneEls:
+#         print(drn.find('serialNumber').text)
 
 
+class xml_parsre:
+    def __init__(self):
+        x = requests.get('http://assignments.reaktor.com/birdnest/drones')
+        root = ET.fromstring(x.content) # Read from string
+        self.root = root
+        self.drones_list = []
+        self.drones_subdic = {}
+        self.device_dic = {}
 
+    def drones(self):
+        # print(self.root.find('deviceInformation').attrib)
+        # print(self.root.find('capture').attrib)
+        # print(self.root.find('capture').get('snapshotTimestamp'))
+        # print(self.root.find('deviceInformation').find('listenRange').text)
+        # self.device_dic2[self.root.find('deviceInformation').attrib.fromkeys]
+        # print(self.root.find('deviceInformation').attrib)
+        # print(self.root.find('deviceInformation').keys()[0])
+        # print(self.root.find('deviceInformation').get('deviceId))
+        self.device_dic = self.root.find('deviceInformation').attrib
+        for deviceInfo in self.root.find('deviceInformation'):
+            self.device_dic[deviceInfo.tag] = deviceInfo.text
+        self.device_dic['snapshotTimestamp'] = self.root.find('capture').get('snapshotTimestamp')
+        # print(self.device_dic)
 
-position = [250000, 250000]
-# position = [400000, 400000]
+        capture = self.root.findall('capture')
+        for el in capture:
+            droneEls = el.findall('drone')
+            for drn in droneEls:
+                for key, drn_subel in enumerate(drn):
+                    # print(key, drn_subel.tag, drn_subel.text)
+                    self.drones_subdic[drn_subel.tag] = drn_subel.text
+                self.drones_list.append(self.drones_subdic)
+                # print(ET.dump(drn))
+                # print(drn.find('serialNumber').text)
+                # print(drn.find('positionY').text)
+        # print(self.drones_list)
+        self.device_dic['drone_list'] = self.drones_list
+        print(self.device_dic)
 
 
 def main():
     monitor = drone_monitor()
-    monitor.monitor_main(*position)
+    get_xml = xml_parsre()
+    get_xml.drones()
+    # monitor.monitor_main(*position)
+    # monitor.monitor_main()
 
 class drone_monitor:
     def __init__(self):
@@ -30,20 +77,33 @@ class drone_monitor:
         self.center_x_protect_area = 250000
         self.center_y_protect_area = 250000   
 
-    def monitor_main(self, drn_pos_x, drn_pos_y):
-        drone_point = self.is_in_protect_area(drn_pos_x, drn_pos_y)
-        if drone_point is not None:
-            print('This drone in no drone zone', drone_point)
+    def monitor_main(self):
+        print('List of drones violating the no-fly zone')
+        # print(self.drones())
+        for drone in self.drones().findall('drone'):
+            serialNumber = drone.find('serialNumber').text
+            drn_pos_x = drone.find('positionX').text
+            drn_pos_y = drone.find('positionY').text
+
+            print(serialNumber, drn_pos_x, drn_pos_y)
+
+            drone_point = self.is_in_protect_area(drn_pos_x, drn_pos_y)
+            if drone_point is not None:
+                print('This drone in no drone zone', drone_point)
 
     def get_drones(self):
         req = requests.get('http://assignments.reaktor.com/birdnest/drones')
-        return req
+        root = ET.fromstring(req.content)
+        return root
     
-    def parse_xml(self):
+    def drones(self):
         drones = self.get_drones()
+        return drones
 
-    # def make_list(self):
-    #     print('sdsd')
+    def make_list(self):
+        print('List of drones violating the no-fly zone')
+
+
 
     def is_in_protect_area(self, drn_pos_x, drn_pos_y):
         res = math.sqrt((drn_pos_x - self.center_x_protect_area) ** 2 + (drn_pos_y - self.center_y_protect_area) ** 2)
