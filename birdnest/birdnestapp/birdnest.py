3ff258b04abacbc312d5ad44b25176f5ff6dbcf9
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-12-26 18:30:44 +0200

    sqlite connection added

--- birdnest/birdnestapp/birdnest.py
+++ birdnest/birdnestapp/birdnest.py
@@ -128,24 +128,27 @@ def main():
     if combine_list:
         print(pandas.DataFrame(combine_list))
         
+
+
     try:
         sqliteConnection = sqlite3.connect('db.sqlite3')
         cursor = sqliteConnection.cursor()
         print("Database created and Successfully Connected to SQLite")
 
-        sqlite_select_Query = "select sqlite_version();"
-        # cursor.execute(sqlite_select_Query)
-        cursor.execute("SELECT * FROM birdnestapp_pilotdata")
+        cursor = sqliteConnection.execute("SELECT * FROM birdnestapp_pilotdata")
+
         record = cursor.fetchall()
         print("SQLite Database Version is: ", record)
         cursor.close()
 
     except sqlite3.Error as error:
         print("Error while connecting to sqlite", error)
+
     finally:
         if sqliteConnection:
             sqliteConnection.close()
             print("The SQLite connection is closed")
+
 
 
 if __name__ == "__main__":
